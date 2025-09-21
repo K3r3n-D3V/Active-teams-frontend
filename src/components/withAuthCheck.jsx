662f6701901d3b460b535829003afa7ba590dec4
Keren Botombe <kerenbotombe125@gmail.com>
2025-09-21 18:09:37 +0200

    added animations

--- src/components/withAuthCheck.jsx
+++ src/components/withAuthCheck.jsx
@@ -1,28 +1,3 @@
-// import React, { useContext, useEffect } from "react";
-// import { useNavigate } from "react-router-dom";
-// import { AuthContext } from "../contexts/AuthContext";
-
-// const withAuthCheck = (WrappedComponent) => {
-//   return function AuthenticatedComponent(props) {
-//     const { user, loading } = useContext(AuthContext);
-//     const navigate = useNavigate();
-
-//     useEffect(() => {
-//       if (!loading && !user) {
-//         navigate("/login");
-//       }
-//     }, [user, loading, navigate]);
-
-//     if (loading || !user) {
-//       return <p>Loading...</p>;
-//     }
-
-//     return <WrappedComponent {...props} />;
-//   };
-// };
-
-// export default withAuthCheck;
-// components/withAuthCheck.js
 import React, { useContext } from "react";
 import { Navigate, useLocation } from "react-router-dom";
 import { AuthContext } from "../contexts/AuthContext";
