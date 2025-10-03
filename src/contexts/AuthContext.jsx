29aecedf134ff44a0fe422e1e02478ec323654df
Keren Botombe <kerenbotombe125@gmail.com>
2025-10-03 12:15:24 +0200

    Changed url

--- src/contexts/AuthContext.jsx
+++ src/contexts/AuthContext.jsx
@@ -101,7 +101,7 @@
 
 import React, { createContext, useState, useEffect } from 'react';
 
-const BACKEND_URL = "http://localhost:8000"
+const BACKEND_URL =  `${import.meta.env.VITE_BACKEND_URL}`;
 
 export const AuthContext = createContext();
 
