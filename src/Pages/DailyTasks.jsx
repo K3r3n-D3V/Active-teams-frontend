f4986cb4f489fb25b647106b2130a895b5eb40d5
GianaTK7 <tkgenia123@gmail.com>
2025-11-14 09:00:08 +0200

    added the required field and disable logic

--- src/Pages/DailyTasks.jsx
+++ src/Pages/DailyTasks.jsx
@@ -1090,6 +1090,7 @@ ${columnWidths.map((width, index) => `                    <x:Column ss:Index="${
               name="taskType"
               value={taskData.taskType}
               onChange={handleChange}
+              required
               style={{
                 width: '100%',
                 padding: '10px 12px',
@@ -1217,6 +1218,7 @@ ${columnWidths.map((width, index) => `                    <x:Column ss:Index="${
                 fetchAssigned(value);
               }}
               autoComplete="off"
+              disabled
               required
               style={{
                 width: '100%',
