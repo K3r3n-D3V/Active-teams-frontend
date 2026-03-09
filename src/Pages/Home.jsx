649f1fa6b813a86c29ff1773dbfb95e5f6343082
Britney Nunes <britneynunes8@gmail.com>
2026-03-09 12:00:33 +0200

    Merging to main

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -15,9 +15,8 @@ const Home = () => {
       setUserName(name || 'User');
       setShowWelcome(true);
       
-      // Clear the flags immediately so it doesn't show again
-      // localStorage.removeItem('showWelcome');
-      // localStorage.removeItem('welcomeUserName');
+      localStorage.removeItem('showWelcome');
+      localStorage.removeItem('welcomeUserName');
     }
   }, []);
 
